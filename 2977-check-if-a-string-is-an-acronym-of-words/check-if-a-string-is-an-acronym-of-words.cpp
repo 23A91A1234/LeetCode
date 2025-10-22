7a7bdb5b083ea54b5b0ff4835691c771666ce5c9
bhavana siva sri <147469401+23A91A1234@users.noreply.github.com>
2025-10-22 20:34:22 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (57.17%) - LeetSync

---
+++ 2977-check-if-a-string-is-an-acronym-of-words/check-if-a-string-is-an-acronym-of-words.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isAcronym(vector<string>& words, string s) {
+        string res="";
+        for(int i=0;i<words.size();i++)
+        {
+            res+=words[i][0];
+        }
+        if(res==s) return  true;
+        return false;
+    }
+};
