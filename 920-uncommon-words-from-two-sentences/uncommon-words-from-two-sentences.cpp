b946cf82a071c2b59bb7f282dc817cf7c4f441b0
bhavana siva sri <147469401+23A91A1234@users.noreply.github.com>
2025-11-15 12:44:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (8.27%) - LeetSync

---
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+vector<string> splitWords(const string &sentence) {
+    stringstream ss(sentence);
+    string word;
+    vector<string> words;
+
+    while (ss >> word) {
+        words.push_back(word);
+    }
+    return words;
+}
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        vector<string>s;
+        vector<string>h;
+        vector<string>res;
+        s=splitWords(s1);
+        h=splitWords(s2);
+        map<string,int>map;
+        for(int i=0;i<s.size();i++)
+        {
+            map[s[i]]++;
+        }
+        for(int i=0;i<h.size();i++)
+        {
+            map[h[i]]++;
+        }
+        for(auto i : map)
+        {
+            if(i.second==1)
+            {
+                res.push_back(i.first);
+            }
+        }
+        return res;
+    }
+};
