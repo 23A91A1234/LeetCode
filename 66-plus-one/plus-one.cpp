76195b69e3797ffa15deeb140c2e861fd3a8c8bb
bhavana siva sri <147469401+23A91A1234@users.noreply.github.com>
2026-01-01 11:13:54 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (96.43%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        vector<int>result;
+        vector<int>res;
         int b=1;
         for(int i=digits.size()-1;i>=0;i--)
         {
-            int h=digits[i]+b;
-            result.push_back(h%10);
+            int h=b+digits[i];
+            res.push_back(h%10);
             b=h/10;
         }
-        if(b!=0)
-        {
-            result.push_back(b);
-        }
-        reverse(result.begin(),result.end());
-        return result;
+        if(b!=0) res.push_back(b);
+        reverse(res.begin(),res.end());
+        return res;
     }
 };
