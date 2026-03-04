a6bb7a3d18364f1e346a00f956a1236f926f08a4
MALIREDDY BHAVANA SIVA SRI <147469401+23A91A1234@users.noreply.github.com>
2026-03-04 14:46:20 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (4.71%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.c
@@ -0,0 +1,10 @@
+int peakIndexInMountainArray(int* arr, int arrSize) {
+    for(int i=0;i<arrSize-1;i++)
+    {
+        if(arr[i]>arr[i+1]&& arr[i-1]<arr[i])
+        {
+            return i;
+        }
+    }
+    return 0;
+}
